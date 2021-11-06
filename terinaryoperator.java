d459c8e57ff0a114a9cd12ed16bc4a979a393ba3
PremPinreddy <pinreddypremchandra046@gmail.com>
2021-11-07 02:31:42 +0530

    terinaryoperator

---
+++ terinaryoperator.java
@@ -0,0 +1,9 @@
+public class terinaryoperator {
+    public static void main(String args[])
+    {
+        int a=2,b=3;
+       int c=a>b?a:b;
+       System.out.println("the value of c is"+c);   
+    }
+    
+}
